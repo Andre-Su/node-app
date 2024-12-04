217d0e8be914887d887eddbf56eb86a064594e86
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2024-12-04 19:51:34 -0300

    update url

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ const moment = require('moment');
 app.listen(8081, function(){
     console.log("Servidor rodando.");
     console.log("porta http://localhost:8081");
-    console.log("porta http://192.168.15.7:8081");
+    console.log("porta http://127.0.0.0:8081");
 });
 
